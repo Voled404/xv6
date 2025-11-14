9a9ae786136b2398b57c7364bb22f3b400cb64d6
Voled404 <voled404@gmail.com>
2025-11-14 11:13:50 +0200

    Created random.h with proper seeding using sys_uptime()

---
+++ include/random.h
@@ -0,0 +1,22 @@
+static unsigned long int next = 1;  // NB: "unsigned long int" is assumed to be 32 bits wide
+
+int rand(void)  // RAND_MAX assumed to be 32767
+{
+    next = next * 1103515245 + 12345;
+    return (unsigned int) (next / 65536) % 32768;
+}
+
+int rand_between(a, b)
+{
+    return (rand() * b) / 32768 + a;
+}
+
+void srand(unsigned int seed)
+{
+    if(next != 1) return;
+    next = seed;
+}
+
+int getseed(void){
+    return next;
+}
